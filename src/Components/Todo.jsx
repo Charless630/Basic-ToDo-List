fd97688d3c0bf0a25af59e175535153a479a621c
Mario <kaszapmario7@gmail.com>
2024-01-01 18:46:02 +0100

    Add local storage functionality to store and retrieve todos

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -10,12 +10,21 @@ export const Todo = () => {
     const add = () => {
         setTodos([...todos, {no:count++, text:inputRef.current.value, display:""}]);
         inputRef.current.value = "";
+        localStorage.setItem("todos_count", count)
     }
 
     useEffect(() => {
-        console.log(todos);
-        localStorage.setItem("todos", JSON.stringify(todos))
+        setTodos(JSON.parse(localStorage.getItem("todos")))
+        count = localStorage.getItem("todos_count")
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(todos);
+            localStorage.setItem("todos", JSON.stringify(todos))
+        }, 100);
     }, [todos])
+    
 
     return (
         <div className='todo'>
