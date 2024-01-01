bb32af85453a27b8492c1bf9322bbd1386dae71f
Mario <kaszapmario7@gmail.com>
2024-01-01 16:38:50 +0100

    Creating Todo basics

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState, useRef, useEffect } from 'react'
+import './CSS/Todo.css'
+
+let count = 0;
+export const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+
+    const add = () => {
+        setTodos([...todos, {no:count++, text:inputRef.current.value, display:""}]);
+        inputRef.current.value = "";
+    }
+
+    useEffect(() => {
+        console.log(todos);
+    }, [todos])
+
+    return (
+        <div className='todo'>
+            <div className='todo-header'>To-Do List</div>
+            <div className='todo-add'>
+                <input ref={inputRef} type='text' placeholder='Add a new task' className='todo-input'/>
+                <div onClick={add} className='todo-add-button'>Add</div>
+            </div>
+            <div className="todo-list"></div>
+        </div>
+    )
+}
